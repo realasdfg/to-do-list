1f27892e9350091d6db25c2cd4b8160d9284b873
Roman <realasdfga@gmail.com>
2026-07-05 18:34:39 +0200

    Install django crispy forms

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "tasks",
 ]
 
@@ -116,5 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL = "tasks.User"
