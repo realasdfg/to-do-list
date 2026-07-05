971cb40f48754753c0e9a55d30fe6c20baf9af12
Roman <realasdfga@gmail.com>
2026-07-05 18:16:53 +0200

    Install django debug toolbar

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tasks.urls", namespace="tasks")),
 ]
+
+urlpatterns += debug_toolbar_urls()
